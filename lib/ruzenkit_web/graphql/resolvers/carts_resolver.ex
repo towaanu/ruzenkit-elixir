8a36abe904e185dfbb5f39e28e9b83b716f1de9b
Antoine Muller <nemak121@gmail.com>
2019-02-16 08:58:25 +0100

    use logged user for creating cart

--- lib/ruzenkit_web/graphql/resolvers/carts_resolver.ex
+++ lib/ruzenkit_web/graphql/resolvers/carts_resolver.ex
@@ -2,6 +2,17 @@ defmodule RuzenkitWeb.Graphql.CartsResolver do
   alias Ruzenkit.Carts
   import Ruzenkit.Utils.Graphql, only: [changeset_error_to_graphql: 2]
 
+
+  def create_cart(_root, %{cart: cart}, %{context: %{current_user: current_user}}) do
+    case Carts.create_cart(%{cart | user_id: current_user.id}) do
+      {:ok, cart} ->
+        {:ok, cart}
+
+      {:error, error} ->
+        {:error, changeset_error_to_graphql("unable to create new cart", error)}
+    end
+  end
+
   def create_cart(_root, %{cart: cart}, _info) do
     case Carts.create_cart(cart) do
       {:ok, cart} ->
