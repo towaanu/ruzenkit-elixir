a44a111c64d3d5dd98296ae28d14061693d4f628
Antoine Muller <nemak121@gmail.com>
2019-05-02 15:53:27 +0200

    update versiin 1.6 => 1.7

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Ruzenkit.MixProject do
   def project do
     [
       app: :ruzenkit,
-      version: "0.1.6",
+      version: "0.1.7",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
